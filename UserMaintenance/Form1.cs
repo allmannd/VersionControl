8d5168d8d29aa8072e1af8fbffe9f4d4bdb9f4b1
David Allmann <allmannd75@gmail.com>
2019-12-01 18:45:55 +0100

    Fifth commit

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +19,7 @@ namespace UserMaintenance
         {
             InitializeComponent();
             label1.Text = Resource1.FullName;
-            
+            button2.Text = Resource1.Write;
             button1.Text = Resource1.Add;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -33,6 +34,28 @@ namespace UserMaintenance
                 
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = "csv";
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() != DialogResult.OK) 
+            {
+                return;
+            }
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8) )
+            {
+                foreach (var item in users)
+                {
+                    sw.Write(item.ID);
+                    sw.Write(";");
+                    sw.Write(item.FullName);
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
